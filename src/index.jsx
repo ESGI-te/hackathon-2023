bad77138635d6f7ee0a8bdac41568afbaa575e1a
tlugat <teo.lugat@ausha.co>
2023-03-30 14:16:58 +0200

    💄 Add MUI theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/globals.css";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider as MuiThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "styled-components";
-import theme from "./utils/theme";
+import { MuiTheme, StyledComponentsTheme } from "./utils/theme";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -22,9 +22,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		<CssBaseline />
 		<BrowserRouter>
 			<QueryClientProvider client={queryClient}>
-				<ThemeProvider theme={theme}>
-					<App />
-				</ThemeProvider>
+				<MuiThemeProvider theme={MuiTheme}>
+					<ThemeProvider theme={StyledComponentsTheme}>
+						<App />
+					</ThemeProvider>
+				</MuiThemeProvider>
 				<ReactQueryDevtools initialIsOpen={false} />
 			</QueryClientProvider>
 		</BrowserRouter>
